cefc7660ddecb51817d1d4d8e2a2cdb9f76bd61c
Egor <eprincev.egor@ya.ru>
2023-08-14 11:21:12 +0300

    fix error  Cannot read properties of null (reading 'indexOf')

--- src/auth/IAuthOptions.ts
+++ src/auth/IAuthOptions.ts
@@ -61,7 +61,7 @@ export type IAuthOptions =
 
 export function isOnPremUrl(siteUrl: string): boolean {
   const host: string = (url.parse(siteUrl)).host;
-  return host.indexOf('.sharepoint.com') === -1 && host.indexOf('.sharepoint.cn') === -1 && host.indexOf('.sharepoint.de') === -1
+  return host && host.indexOf('.sharepoint.com') === -1 && host.indexOf('.sharepoint.cn') === -1 && host.indexOf('.sharepoint.de') === -1
     && host.indexOf('.sharepoint-mil.us') === -1 && host.indexOf('.sharepoint.us') === -1;
 }
 
